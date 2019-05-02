8c7767223086523e968f211329a1335b186fabb8
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 20:39:45 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,8 @@
 class Song < ApplicationRecord
+  validates :title, presence: true 
+  validates :title, uniqueness: true 
+  validates :released, inclusion: {in: [true, false] } 
+  validates :release_year, presence: true 
+  validates_numericality_of :release_year { less_than_or_equal_to: 2019 }
+  validates :artist_name, presence: true 
 end
